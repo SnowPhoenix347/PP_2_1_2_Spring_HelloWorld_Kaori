6d1822953c77cd8877691cc7c45ca4e3297289ef
snowphoenix347 <snowphoenix347@gmail.com>
2024-09-25 16:30:35 +0300

    Практическая задача 2.1.2 Spring. Первые шаги. HelloWorld.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Hello world equals: " + bean.equals(bean2));
+        System.out.println("Cat equals: " + cat.equals(cat2));
         System.out.println(bean.getMessage());
+        System.out.println(cat.getMessage());
     }
 }
